5514ee62c0dcfd2e4d547101d5aa34c199eaeea4
andros230 <andros230@126.com>
2016-06-10 04:15:30 +0800

    Initial commit

---
+++ app/src/main/java/com/andros230/basicmapactivity/MainActivity.java
@@ -0,0 +1,43 @@
+package com.andros230.basicmapactivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.amap.api.maps.MapView;
+
+public class MainActivity extends Activity {
+    private MapView mMapView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mMapView = (MapView) findViewById(R.id.map);
+        mMapView.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mMapView.onDestroy();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mMapView.onResume();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mMapView.onSaveInstanceState(outState);
+    }
+}
