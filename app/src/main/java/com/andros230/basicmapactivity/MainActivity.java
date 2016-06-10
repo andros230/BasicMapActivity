7319ed961a0043cf34abc99f6c5bebdaa941d4f3
andros230 <andros230@126.com>
2016-06-10 22:59:39 +0800

    添加图标标注

--- app/src/main/java/com/andros230/basicmapactivity/MainActivity.java
+++ app/src/main/java/com/andros230/basicmapactivity/MainActivity.java
@@ -3,10 +3,17 @@ package com.andros230.basicmapactivity;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.amap.api.maps.AMap;
 import com.amap.api.maps.MapView;
+import com.amap.api.maps.model.BitmapDescriptorCreator;
+import com.amap.api.maps.model.BitmapDescriptorFactory;
+import com.amap.api.maps.model.LatLng;
+import com.amap.api.maps.model.Marker;
+import com.amap.api.maps.model.MarkerOptions;
 
 public class MainActivity extends Activity {
     private MapView mMapView;
+    private AMap aMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +22,17 @@ public class MainActivity extends Activity {
 
         mMapView = (MapView) findViewById(R.id.map);
         mMapView.onCreate(savedInstanceState);
+
+
+        if (aMap == null) {
+            aMap = mMapView.getMap();
+        }
+
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(new LatLng(31.20998,121.476077));
+        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE));
+        Marker marker = aMap.addMarker(markerOptions);
+
     }
 
     @Override
